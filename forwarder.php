491a956869932f6009a9b62a7993e5aed3d52ac8
reneade <reneade@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-19 23:11:29 +0000

    added forwarder to fit the stupid wordpress plugins folder structure
    git-svn-id: https://plugins.svn.wordpress.org/sensitive-tag-cloud/trunk@127569 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ forwarder.php
@@ -0,0 +1,16 @@
+<?php
+//-----------------------------------------------------------------------------
+/*
+Plugin Name: Sensitive Tag Cloud
+Version: 1.3.1
+Plugin URI: http://www.rene-ade.de/inhalte/wordpress-plugin-sensitivetagcloud.html
+Description: This wordpress plugin provides a highly configurable tagcloud that shows tags depending of the current context.
+Author: Ren&eacute; Ade
+Author URI: http://www.rene-ade.de
+Min WP Version: 2.3
+*/
+//-----------------------------------------------------------------------------
+?>
+<?php
+  include 'sensitive-tag-cloud/sensitive-tag-cloud.php';
+?>
